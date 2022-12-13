66fb150c6817220bc202b4e69ebab42579f12881
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2022-12-13 20:18:50 -0300

    update subject

--- database/migrations/2022_12_12_235929_subject.php
+++ database/migrations/2022_12_12_235929_subject.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('subjects', function (Blueprint $table) {
             $table->id()->autoIncrement();
-            $table->string('name');
+            $table->string('title');
             $table->string('description');
             $table->integer('period');
             $table->timestamp('created_at')->nullable();
