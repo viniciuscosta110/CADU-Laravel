442d740dd4150bf28caab9abf92d46086bc0ca0d
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2022-12-14 21:32:16 -0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ Route::get('/details', function () {
 
 Route::get('/extensions', [ExtensionController::class, 'index']);
 
-Route::get('/login',[loginController::class, 'login']);
+Route::get('/login',[loginController::class, 'index']);
 Route::post('/login',[loginController::class, 'login']);
 
 Route::get('/subjects', function () {
